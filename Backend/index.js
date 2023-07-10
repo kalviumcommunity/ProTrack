24aa4c9c301ee2df16ae3baf2a78070405e22e66
AmanNinave <amanninave987@gmail.com>
2023-07-10 14:40:36 +0530

    basic functionalities of home Page

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,14 @@
 // const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
-
+const pickerRoute = require('./Routes/picker.route')
 let app = express();
 let connectToDatabase = require('./db/ConnectDatabase')
 
 app.use(express.json());
 app.use(cors());
 
-
+app.use('/picker' , pickerRoute );
 
 connectToDatabase().then(() => {
 
